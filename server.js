f8e1f4813333f8fbf31329823a5da821478097fb
Praveen8104 <praveenreddygoli8@gmail.com>
2025-08-16 20:57:46 +0530

    fix: Add node-cron dependency for scheduled tasks

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = 3000;
+const cron = require('node-cron');
 
 app.use(cors());
 app.use(express.json());
